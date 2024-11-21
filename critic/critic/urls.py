9748aa6fa26deccc4acddb65e58b3de1cd77b978
nschantz21 <nschantz21@gmail.com>
2024-11-21 00:05:55 +0000

    adding user models

--- critic/critic/urls.py
+++ critic/critic/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('myapp.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('myapp/', include('myapp.urls')),
 ]
