72223da3ee6295cda93020e8aa4d03cd0a327481
Nicholas Schantz <nschantz21@gmail.com>
2025-01-21 22:59:30 -0500

    Update settings.py

--- critic/critic/settings.py
+++ critic/critic/settings.py
@@ -197,10 +197,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "critic/static"]
+STATIC_ROOT = "static"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = []
 
 
 # Default primary key field type
